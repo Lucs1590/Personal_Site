c99965674bfc5526f3084bacf273a5fe58fc95d9
LucasBrito <lucasbsilva29@gmail.com>
2019-07-26 20:00:58 -0300

    bootstrap and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MHomeComponent } from './m-home/m-home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'm_home', component: MHomeComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
