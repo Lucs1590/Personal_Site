0c7ef24f1555d6f83bdb329b108dcf8e1e33cb55
Lucas Brito <lucasbsilva29@gmail.com>
2019-11-22 15:36:29 -0300

    Trocando o m_home pois irei utilizar tudo reponsivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
-import { MHomeComponent } from './m-home/m-home.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MHomeComponent
   ],
   imports: [
     BrowserModule,
