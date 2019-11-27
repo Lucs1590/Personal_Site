e6374a87ac8feed18e27ed39fc0602aa6574cd0d
Lucas Brito <lucasbsilva29@gmail.com>
2019-11-26 21:25:01 -0300

    Colocando as atigas configurações do m-home

--- src/app/m-home/m-home.component.ts
+++ src/app/m-home/m-home.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./m-home.component.css']
 })
 export class MHomeComponent implements OnInit {
+  TEXT_TRANS = 'en';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  translate() {
+    if (this.TEXT_TRANS.match('pt')) {
+      this.TEXT_TRANS = 'en';
+    } else {
+      this.TEXT_TRANS = 'pt';
+    }
   }
 
 }
