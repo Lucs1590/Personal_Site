834fec2e0c1dc571c4295a91385de4e14343db7a
Lucas Brito <lucasbsilva29@gmail.com>
2019-12-28 00:34:06 -0300

    Atualizando a versão mobile

--- src/app/m-home/m-home.component.ts
+++ src/app/m-home/m-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-m-home',
@@ -6,19 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./m-home.component.css']
 })
 export class MHomeComponent implements OnInit {
-  TEXT_TRANS = 'en';
-
-  constructor() { }
+  CURRENT_LANG = 'pt';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('pt');
+  }
 
   ngOnInit() {
   }
 
-  translate() {
-    if (this.TEXT_TRANS.match('pt')) {
-      this.TEXT_TRANS = 'en';
+  useLanguage() {
+    if (this.CURRENT_LANG.match('pt')) {
+      this.CURRENT_LANG = 'en';
+      this.translate.use(this.CURRENT_LANG);
     } else {
-      this.TEXT_TRANS = 'pt';
+      this.CURRENT_LANG = 'pt';
+      this.translate.use(this.CURRENT_LANG);
     }
   }
-
 }
