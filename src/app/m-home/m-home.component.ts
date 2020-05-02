ee04879ba94ae6b41dca65a4ece1e7df4f3bb3e2
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-02 12:35:23 -0300

    colocando animação na foto de entrada e no titulo

--- src/app/m-home/m-home.component.ts
+++ src/app/m-home/m-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -6,13 +6,21 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './m-home.component.html',
   styleUrls: ['./m-home.component.css']
 })
-export class MHomeComponent implements OnInit {
+export class MHomeComponent implements OnInit, AfterContentInit {
   CURRENT_LANG = 'pt';
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('pt');
   }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit(): void {
+    const title = document.getElementById('title_name');
+    const subtitle = document.getElementById('sub_title');
+
+    title.classList.add('animated', 'zoomIn');
+    subtitle.classList.add('animated', 'zoomIn');
   }
 
   useLanguage() {
