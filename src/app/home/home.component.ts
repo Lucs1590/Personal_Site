f4d927486834e3a20df560c7d585c6b23ab9eead
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-02 12:35:01 -0300

    boas praticas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit, AfterContentInit {
     const image2 = 'url("../../assets/img/principal-min.jpg")';
 
     const title = document.getElementById('title_name');
-    const subtitle = document.getElementById('sub_title')
+    const subtitle = document.getElementById('sub_title');
     const elemento = document.getElementsByClassName('imgPrincipal') as HTMLCollectionOf<HTMLElement>;
 
     elemento[0].style['background-image'] = window.innerWidth >= 1350 ? image1 : image2;
