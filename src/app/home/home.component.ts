e5ce02ffa463f50023b42f404dd1c3c7b1522a60
Lucas Brito <lucasbsilva29@gmail.com>
2020-05-01 22:44:14 -0300

    animações

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,9 +20,16 @@ export class HomeComponent implements OnInit, AfterContentInit {
   ngAfterContentInit(): void {
     const image1 = 'url("../../assets/img/perfil_site.png")';
     const image2 = 'url("../../assets/img/principal-min.jpg")';
+
+    const title = document.getElementById('title_name');
+    const subtitle = document.getElementById('sub_title')
+
     const elemento = document.getElementsByClassName('imgPrincipal') as HTMLCollectionOf<HTMLElement>;
     elemento[0].style['background-image'] = window.innerWidth >= 1350 ? image1 : image2;
     elemento[0].classList.add('animated', 'fadeInUp');
+    title.classList.add('animated', 'fadeInLeft');
+    subtitle.classList.add('animated', 'fadeInLeft');
+    
   }
 
   useLanguage() {
