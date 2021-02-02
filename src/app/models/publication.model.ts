4a57f9544121d3ce7bf580a7ac5542e46125d53a
Lucas Brito <lucasbsilva29@gmail.com>
2021-02-02 15:17:03 -0300

    refactor(publication.model.ts): optional chaining and indentation

--- src/app/models/publication.model.ts
+++ src/app/models/publication.model.ts
@@ -1,33 +1,33 @@
 import { Deserializable } from './deserializable.model';
 
 export class Publication implements Deserializable {
-    title: string;
-    description: string;
-    image: string;
-    publicationDate: Date;
-    url: string;
-    author: string;
+  title?: string;
+  description?: string;
+  image?: string;
+  publicationDate?: Date;
+  url?: string;
+  author?: string;
 
-    deserialize(input: {
-        title: string;
-        pubDate: string | Date;
-        link: string;
-        guid: string;
-        author: string;
-        thumbnail: string;
-        description: string;
-        content: string;
-        enclosure: any;
-        categories: string[];
-    }): this {
-        Object.assign(this, {});
-        this.title = input.title;
-        this.description = input.description.
-            slice(input.description.indexOf('<p>'), input.description.indexOf('</p>') + 4).concat('...');
-        this.image = input.thumbnail;
-        this.publicationDate = new Date(input.pubDate);
-        this.url = input.link;
-        this.author = input.author;
-        return this;
-    }
+  deserialize(input: {
+    title: string;
+    pubDate: string | Date;
+    link: string;
+    guid: string;
+    author: string;
+    thumbnail: string;
+    description: string;
+    content: string;
+    enclosure: any;
+    categories: string[];
+  }): this {
+    Object.assign(this, {});
+    this.title = input?.title;
+    this.description = input?.description.
+      slice(input?.description.indexOf('<p>'), input?.description.indexOf('</p>') + 4).concat('...');
+    this.image = input?.thumbnail;
+    this.publicationDate = new Date(input?.pubDate);
+    this.url = input?.link;
+    this.author = input?.author;
+    return this;
+  }
 }
