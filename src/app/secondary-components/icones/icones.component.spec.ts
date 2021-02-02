0ddf22e6eea3e3f3f869ac751da9fa255100e215
Lucas Brito <lucasbsilva29@gmail.com>
2021-02-02 16:31:21 -0300

    build: update angular to v11

--- src/app/secondary-components/icones/icones.component.spec.ts
+++ src/app/secondary-components/icones/icones.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { IconesComponent } from './icones.component';
 
@@ -6,7 +6,7 @@ describe('IconesComponent', () => {
   let component: IconesComponent;
   let fixture: ComponentFixture<IconesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ IconesComponent ]
     })
