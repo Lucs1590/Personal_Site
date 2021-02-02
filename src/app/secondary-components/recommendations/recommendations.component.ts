8a175ac0672ebebf58977ef192e2aed713ee0982
Lucas Brito <lucasbsilva29@gmail.com>
2021-02-02 17:06:57 -0300

    feat(recommendations.component.ts): add all recommendations

--- src/app/secondary-components/recommendations/recommendations.component.ts
+++ src/app/secondary-components/recommendations/recommendations.component.ts
@@ -13,27 +13,33 @@ export class RecommendationsComponent {
   recommendations = [
     {
       author: 'Samuel Licorio Leiva',
-      authorImage: 'src/assets/img/samu.jpeg',
+      authorImage: '../../assets/img/samu.jpeg',
       text: 'Profissional com uma índole admirável, além de um conhecimento técnico apurado e sua empatia impecável, sempre se colocando no lugar dos outros, o que o leva a refletir muito sobre suas atitudes. Isso o torna uma pessoa incrível e extremamente agradável para se trabalhar em equipe. Sou muito grato a Deus por todas as oportunidades profissionais e pessoais em que já convivemos. Grande e fraternal abraço, amado.',
-      stage: ''
+      stage: 'Em 7 de abril de 2020, Samuel trabalhava com Lucas, mas em grupos diferentes'
     },
     {
       author: 'Álvaro Leandro Cavalvante',
-      authorImage: 'src/assets/img/alvaro.jpeg',
+      authorImage: '../../assets/img/alvaro.jpeg',
       text: 'Ótimo profissional, muito dedicado em suas tarefas, visa executar tudo com excelência, se mostrando um profissional muito comprometido com o trabalho.',
-      stage: ''
+      stage: 'Em 17 de setembro de 2019, Lucas trabalhava com Álvaro no mesmo grupo'
     },
     {
       author: 'Fernando Maransatto',
-      authorImage: 'src/assets/img/maransatto.jpeg',
+      authorImage: '../../assets/img/maransatto.jpeg',
       text: 'Excelente profissional. Responsável, não abre mão de desafio, encara bem as mudanças de escopo. Tem uma ótima curva de aprendizado, e trabalha muito bem em equipe.',
-      stage: ''
+      stage: 'Em 18 de setembro de 2019, Fernando respondia a Lucas diretamente'
     },
     {
       author: 'Renan Zulian',
-      authorImage: 'src/assets/img/renan.jpeg',
-      text: 'Fui e sempre será um prazer dividir uma mesa de trabalho com ele. Uma pessoa dedicada, que tem um ótimo espírito competitivo e que ama desafios. Esse seu estilo sempre motiva os colegas de trabalho a fazer o seu melhor.',
-      stage: ''
+      authorImage: '../../assets/img/renan.jpeg',
+      text: 'Foi e sempre será um prazer dividir uma mesa de trabalho com ele. Uma pessoa dedicada, que tem um ótimo espírito competitivo e que ama desafios. Esse seu estilo sempre motiva os colegas de trabalho a fazer o seu melhor.',
+      stage: 'Em 12 de fevereiro de 2020, Lucas trabalhava com Renan no mesmo grupo'
+    },
+    {
+      author: 'Rafael Petraca',
+      authorImage: '../../assets/img/rafa.jpeg',
+      text: 'Ótimo profissional, com valores e ética. Sempre compartilhando conhecimento com os demais! E acima de tudo um grande amigo.',
+      stage: 'Em 2 de fevereiro de 2021, Lucas trabalhava com Rafael no mesmo grupo'
     }
   ].map(recommendation => new Recommendation().deserialize(recommendation));
 
