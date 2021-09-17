508d9c073619e89f831984f6324ec4bbfcad15d9
Lucas Brito <lucasbsilva29@gmail.com>
2021-09-17 17:45:20 -0300

    feat: add seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) { }
+
+  ngOnInit(): void {
+    this.title.setTitle("Lucas Brito")
+    const date = new Date(Date.now());
+    let formatedData = ((date.getDate())) + "/" + ((date.getMonth() + 1)) + "/" + date.getFullYear();
+    this.meta.addTags([
+      { name: 'description', content: 'Lucas Brito homepage.' },
+      { name: 'author', content: 'Lucs1590' },
+      { name: 'keywords', content: 'Brito, Lucs1590, Development, IA, Machine Learning, Fullstack' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: formatedData, scheme: 'DD-MM-YYYY' },
+      { charset: 'UTF-8' }
+    ]);
+  }
 }
