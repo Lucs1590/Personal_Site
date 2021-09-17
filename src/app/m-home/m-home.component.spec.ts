4bdd167af28f5c16eec433531cf3ff0d9f0e32ec
Lucas Brito <lucasbsilva29@gmail.com>
2021-09-17 15:35:49 -0300

    build: updating to 12

--- src/app/m-home/m-home.component.spec.ts
+++ src/app/m-home/m-home.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MHomeComponent } from './m-home.component';
 
@@ -6,7 +6,7 @@ describe('MHomeComponent', () => {
   let component: MHomeComponent;
   let fixture: ComponentFixture<MHomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MHomeComponent ]
     })
