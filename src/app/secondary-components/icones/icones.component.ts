1fc85472512d7811961edd04ccf4d45f889ab100
Lucas Brito <lucasbsilva29@gmail.com>
2021-09-18 20:18:12 -0300

    fix(icones.component.ts): set type and fix url

--- src/app/secondary-components/icones/icones.component.ts
+++ src/app/secondary-components/icones/icones.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Icon } from 'src/app/models/icon.model';
 
 @Component({
   selector: 'app-icones',
@@ -17,47 +18,48 @@ export class IconesComponent implements OnInit {
     },
     {
       icon: ['fab', 'facebook-f'],
-      link: 'https://www.facebook.com/lucas.brito.100"',
+      link: 'https://www.facebook.com/lucas.brito.100',
       active: true
     },
     {
       icon: ['fab', 'medium'],
-      link: 'https://medium.com/@lucasbsilva29"',
+      link: 'https://medium.com/@lucasbsilva29',
       active: true
     },
     {
       icon: ['fab', 'twitter'],
-      link: 'https://twitter.com/Lucs1590"',
+      link: 'https://twitter.com/Lucs1590',
       active: true
     },
     {
       icon: ['fab', 'linkedin'],
-      link: 'https://www.linkedin.com/in/lucas-brito100/"',
+      link: 'https://www.linkedin.com/in/lucas-brito100/',
       active: true
     },
     {
       icon: ['fab', 'github'],
-      link: 'https://github.com/Lucs1590"',
+      link: 'https://github.com/Lucs1590',
       active: true
     },
     {
       icon: ['fab', 'stack-overflow'],
-      link: 'https://pt.stackoverflow.com/users/122718/lucas-brito"',
+      link: 'https://pt.stackoverflow.com/users/122718/lucas-brito',
       active: true
     },
     {
       icon: ['fab', 'whatsapp'],
-      link: '"https://api.whatsapp.com/send?phone=5514997270606&text=Oi%20Lucas"',
+      link: 'https://api.whatsapp.com/send?phone=5514997270606&text=Oi%20Lucas',
       active: false
     },
     {
       icon: ['fab', 'instagram'],
-      link: 'https://www.instagram.com/_lucasbritoo"',
+      link: 'https://www.instagram.com/_lucasbritoo',
       active: true
     }
   ]
 
   ngOnInit(): void {
+    this.iconList?.map(icon => new Icon().deserialize(icon));
   }
 
 }
