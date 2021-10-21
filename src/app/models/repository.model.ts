c5b4924a51c92042cc0722c548a142e1c7e05328
Lucas Brito <lucasbsilva29@gmail.com>
2021-10-21 00:04:36 -0300

    feat(repository.model.ts): add model to repositories

---
+++ src/app/models/repository.model.ts
@@ -0,0 +1,31 @@
+import { Deserializable } from './deserializable.model';
+
+export class Repository implements Deserializable {
+    public name: string;
+    public stars: number;
+    public forks: number;
+    public issues: number;
+    public url: string;
+    public topics: string[];
+    public language: string;
+    public description: string;
+    public created_at: Date;
+    public updateDate: Date;
+    public private: boolean;
+
+    deserialize(input: any): this {
+        this.name = input?.name;
+        this.stars = input?.stargazers_count;
+        this.forks = input?.forks_count;
+        this.issues = input?.open_issues_count;
+        this.url = input?.url;
+        this.topics = input?.topics;
+        this.language = input?.language;
+        this.description = input?.description;
+        this.created_at = new Date(input?.created_at);
+        this.updateDate = new Date(input?.updated_at);
+        this.private = input?.private;
+
+        return this;
+    }
+}
