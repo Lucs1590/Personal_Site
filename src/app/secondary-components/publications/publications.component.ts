645be8dd1e0fa48f2d6e31670cc39a4ea97c3a97
Lucas Brito <lucasbsilva29@gmail.com>
2023-01-08 16:21:06 -0300

    feat: google scholar icon

--- src/app/secondary-components/publications/publications.component.ts
+++ src/app/secondary-components/publications/publications.component.ts
@@ -10,17 +10,28 @@ import { ApiService } from 'src/app/services/api.service';
 export class PublicationsComponent implements OnInit {
   publications: Publication[];
   loading = false;
+  scholarImage: string;
 
   constructor(
     private apiService: ApiService
   ) { }
 
   async ngOnInit() {
+    this.defineIconImage(null);
     await this.getPublications();
     setTimeout(() => {
       this.loading = true;
     }, 600);
   }
+
+  public defineIconImage(event: MouseEvent): void {
+    if (event && event.type === 'mouseover') {
+      this.scholarImage = '../../../assets/img/google-scholar2.svg';
+    } else {
+      this.scholarImage = '../../../assets/img/google-scholar1.svg';
+    }
+  }
+
 
   async getPublications() {
     const publications = await this.apiService.getAllPublications().toPromise();
