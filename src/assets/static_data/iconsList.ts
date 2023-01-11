22b5a88df68da7a0071a4a9f9d50a82093b39d74
Lucas Brito <lucasbsilva29@gmail.com>
2023-01-11 18:58:33 -0300

    refactor: exporting const from static list

---
+++ src/assets/static_data/iconsList.ts
@@ -0,0 +1,51 @@
+export const iconList = [
+    {
+        icon: ['fas', 'envelope'],
+        link: 'mailto:lucasbsilva29@gmail.com?subject=Brito%20Web%20Site&body=Ola%20Lucas%20Brito',
+        active: true
+    }, {
+        icon: ['fab', 'github'],
+        link: 'https://github.com/Lucs1590',
+        active: true
+    },
+    {
+        icon: ['fab', 'stack-overflow'],
+        link: 'https://pt.stackoverflow.com/users/122718/lucas-brito',
+        active: true
+    },
+    {
+        icon: ['fab', 'medium'],
+        link: 'https://medium.com/@lucasbsilva29',
+        active: true
+    },
+    {
+        icon: ['fab', 'dev'],
+        link: 'https://dev.to/lucs1590',
+        active: true
+    },
+    {
+        icon: ['fab', 'twitter'],
+        link: 'https://twitter.com/Lucs1590',
+        active: true
+    },
+    {
+        icon: ['fab', 'linkedin'],
+        link: 'https://www.linkedin.com/in/lucas-brito100/',
+        active: true
+    },
+    {
+        icon: ['fab', 'instagram'],
+        link: 'https://www.instagram.com/_lucasbritoo',
+        active: true
+    },
+    {
+        icon: ['fab', 'facebook-f'],
+        link: 'https://www.facebook.com/lucas.brito.100',
+        active: true
+    },
+    {
+        icon: ['fab', 'whatsapp'],
+        link: 'https://api.whatsapp.com/send?phone=5514997270606&text=Oi%20Lucas',
+        active: false
+    },
+]
