10edd339a2aa9ea755556242df3cf32efffb0039
Lucas Brito <lucasbsilva29@gmail.com>
2023-01-12 15:36:08 -0300

    feat: add initial technologies list

--- src/app/secondary-components/technologies-list/technologies-list.component.ts
+++ src/app/secondary-components/technologies-list/technologies-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Technology } from 'src/app/models/technology.model';
+import { techList } from 'src/assets/static_data/techList';
+
+
 
 @Component({
   selector: 'app-technologies-list',
   templateUrl: './technologies-list.component.html',
   styleUrls: ['./technologies-list.component.css']
 })
-export class TechnologiesListComponent {
+export class TechnologiesListComponent implements OnInit, AfterViewInit {
+  public technologiesList: Technology[] = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.technologiesList = techList.map(icon => new Technology().deserialize(icon));
+  }
+
+  async ngAfterViewInit(): Promise<void> { }
+
+  public openLink(link: string): void {
+    window.open(link, '_blank').focus();
+  }
 }
