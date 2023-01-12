8c0d99524d6aa97c831116133056a0be71b802bf
Lucas Brito <lucasbsilva29@gmail.com>
2023-01-12 10:31:38 -0300

    feat: creating technology list with model

---
+++ src/assets/static_data/techList.ts
@@ -0,0 +1,135 @@
+export const techList = [
+    {
+        'id': 1,
+        'name': 'Angular',
+        'image': 'https://angular.io/assets/images/logos/angular/angular.svg',
+        'description': 'Angular is a TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.',
+        'link': 'https://angular.io/'
+    },
+    {
+        'id': 2,
+        'name': 'Python',
+        'image': 'https://www.python.org/static/community_logos/python-logo-master-v3-TM.png',
+        'description': 'Python is an interpreted, high-level and general-purpose programming language. Python\'s design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.',
+        'link': 'https://www.python.org/'
+    },
+    {
+        'id': 3,
+        'name': 'Ionic',
+        'image': 'https://ionicframework.com/img/meta/ionic-framework-og.png',
+        'description': 'Ionic is a complete open-source SDK for hybrid mobile app development. Ionic Framework is an open source UI toolkit for building performant, high-quality mobile and desktop apps using web technologies (HTML, CSS, and JavaScript).',
+        'link': 'https://ionicframework.com/'
+    },
+    {
+        'id': 4,
+        'name': 'FastAPI',
+        'image': 'https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png',
+        'description': 'FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints. It is easy to learn, fast to code, ready for production, and scales up easily.',
+        'link': 'https://fastapi.tiangolo.com/'
+    },
+    {
+        'id': 5,
+        'name': 'OpenCV',
+        'image': 'https://opencv.org/wp-content/uploads/2020/07/cropped-OpenCV_logo_white_600x.png',
+        'description': 'OpenCV (Open Source Computer Vision Library) is an open source computer vision and machine learning software library. OpenCV was built to provide a common infrastructure for computer vision applications and to accelerate the use of machine perception in the commercial products. Being a BSD-licensed product, OpenCV makes it easy for businesses to utilize and modify the code.',
+        'link': 'https://opencv.org/'
+    },
+    {
+        'id': 6,
+        'name': 'NodeJS',
+        'image': 'https://nodejs.org/static/images/logo.svg',
+        'description': 'Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.',
+        'link': 'https://nodejs.org/en/'
+    },
+    {
+        'id': 7,
+        'name': 'Express.js',
+        'image': 'https://expressjs.com/images/express-facebook-share.png',
+        'description': 'Express.js, or simply Express, is a back end web application framework for Node.js, released as free and open-source software under the MIT License. It is designed for building web applications and APIs. It has been called the de facto standard server framework for Node.js.',
+        'link': 'https://expressjs.com/'
+    },
+    {
+        'id': 8,
+        'name': 'Electron',
+        'image': 'https://www.electronjs.org/images/electron-logo.svg',
+        'description': 'Electron is an open-source software framework developed and maintained by GitHub. It allows for the development of desktop GUI applications using web technologies: it combines the Chromium rendering engine and the Node.js runtime.',
+        'link': 'https://www.electronjs.org/'
+    },
+    {
+        'id': 9,
+        'name': 'Pandas',
+        'image': 'https://pandas.pydata.org/static/img/pandas_mark.svg',
+        'description': 'Pandas is a fast, powerful, flexible and easy to use open source data analysis and manipulation tool, built on top of the Python programming language.',
+        'link': 'https://pandas.pydata.org/'
+    },
+    {
+        'id': 10,
+        'name': 'Numpy',
+        'image': 'https://numpy.org/images/logos/numpy.svg',
+        'description': 'NumPy is the fundamental package for scientific computing with Python. It contains among other things: a powerful N-dimensional array object, sophisticated (broadcasting) functions, tools for integrating C/C++ and Fortran code, useful linear algebra, Fourier transform, and random number capabilities.',
+        'link': 'https://numpy.org/'
+    },
+    {
+        'id': 11,
+        'name': 'Matplotlib',
+        'image': 'https://matplotlib.org/_static/logo2_compressed.svg',
+        'description': 'Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations in Python. Matplotlib makes easy things easy and hard things possible. You can generate plots, histograms, power spectra, bar charts, errorcharts, scatterplots, etc., with just a few lines of code.',
+        'link': 'https://matplotlib.org/'
+    },
+    {
+        'id': 12,
+        'name': 'Sklearn',
+        'image': 'https://scikit-learn.org/stable/_static/scikit-learn-logo-small.png',
+        'description': 'Scikit-learn is a free software machine learning library for the Python programming language. It features various classification, regression and clustering algorithms including support vector machines, random forests, gradient boosting, k-means and DBSCAN, and is designed to interoperate with the Python numerical and scientific libraries NumPy and SciPy.',
+        'link': 'https://scikit-learn.org/stable/'
+    },
+    {
+        'id': 13,
+        'name': 'Tensorflow',
+        'image': 'https://www.tensorflow.org/images/tf_logo_social.png',
+        'description': 'TensorFlow is an end-to-end open source platform for machine learning. It has a comprehensive, flexible ecosystem of tools, libraries, and community resources that lets researchers push the state-of-the-art in ML and developers easily build and deploy ML-powered applications.',
+        'link': 'https://www.tensorflow.org/'
+    },
+    {
+        'id': 14,
+        'name': 'Keras',
+        'image': 'https://keras.io/img/logo.png',
+        'description': 'Keras is a high-level neural networks API, written in Python and capable of running on top of TensorFlow, CNTK, or Theano. It was developed with a focus on enabling fast experimentation. Being able to go from idea to result with the least possible delay is key to doing good research.',
+        'link': 'https://keras.io/'
+    },
+    {
+        'id': 15,
+        'name': 'MySQL',
+        'image': 'https://www.mysql.com/common/logos/logo-mysql-170x115.png',
+        'description': 'MySQL is an open-source relational database management system (RDBMS). Its name is a combination of "My", the name of co-founder Michael Widenius\'s daughter, and "SQL", the abbreviation for Structured Query Language.',
+        'link': 'https://www.mysql.com/'
+    },
+    {
+        'id': 16,
+        'name': 'MongoDB',
+        'image': 'https://www.mongodb.com/assets/images/global/leaf.png',
+        'description': 'MongoDB is a cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License.',
+        'link': 'https://www.mongodb.com/'
+    },
+    {
+        'id': 17,
+        'name': 'Neo4j',
+        'image': 'https://neo4j.com/wp-content/uploads/neo4j_logo_whitebackground-1.png',
+        'description': 'Neo4j is a graph database management system developed by Neo4j, Inc. Described by its developers as an ACID-compliant transactional database with native graph storage and processing, Neo4j is the most popular graph database according to DB-Engines ranking.',
+        'link': 'https://neo4j.com/'
+    },
+    {
+        'id': 18,
+        'name': 'Docker',
+        'image': 'https://www.docker.com/sites/default/files/d8/2019-07/vertical-logo-monochromatic.png',
+        'description': 'Docker is a set of platform as a service (PaaS) products that use OS-level virtualization to deliver software in packages called containers. Containers are isolated from one another and bundle their own software, libraries and configuration files; they can communicate with each other through well-defined channels.',
+        'link': 'https://www.docker.com/'
+    },
+    {
+        'id': 19,
+        'name': 'Qt',
+        'image': 'https://www.qt.io/qt-static/press-kit/images/qt-logo.png',
+        'description': 'Qt is a cross-platform application framework that is used for developing application software that can be run on various software and hardware platforms with little or no change in the underlying codebase, while still being a native application with native capabilities and speed.',
+        'link': 'https://www.qt.io/'
+    }
+]
