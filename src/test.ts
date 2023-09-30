a649dfff68019afa197aed00de67aa5c0d997219
Lucas Brito <lucasbsilva29@gmail.com>
2023-09-30 12:41:44 -0300

    style: indentation

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,3 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
@@ -11,6 +9,6 @@ import {
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
+  teardown: { destroyAfterEach: false }
 }
 );
