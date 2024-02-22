38ac982820c52a9d521e42a4abbeb33133ecb907
Lucas Brito <lucasbsilva29@gmail.com>
2024-02-21 22:36:19 -0300

    feat: improve website SEO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,23 +20,23 @@ export class AppComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.setLanguage();
-    this.title.setTitle('Lucas Brito');
+    this.title.setTitle('Lucas Brito - Personal Website');
     const date = new Date(Date.now());
 
-    const formatedData = `${(date.getDate())}/${(date.getMonth() + 1)}/${date.getFullYear()}`;
+    const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
     this.meta.addTags([
-      { name: 'description', content: 'Lucas Brito' },
+      { name: 'description', content: 'Lucas Brito - Personal Website. AI, Machine Learning, Data' },
       { name: 'author', content: 'Lucs1590' },
-      { name: 'keywords', content: 'Lucas Brito, Lucs1590, AI, IA, Machine Learning' },
+      { name: 'keywords', content: 'Lucas Brito, Lucs1590, AI, Machine Learning, Personal Website, Data' },
       { name: 'robots', content: 'index, follow' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'date', content: formatedData, scheme: 'DD/MM/YYYY' },
-      { name: 'revised', content: formatedData, scheme: 'DD/MM/YYYY' },
-      { name: 'og:title', content: 'Lucas Brito' },
+      { name: 'date', content: formattedDate, scheme: 'DD/MM/YYYY' },
+      { name: 'revised', content: formattedDate, scheme: 'DD/MM/YYYY' },
+      { name: 'og:title', content: 'Lucas Brito - Personal Website' },
       { name: 'og:type', content: 'website' },
       { name: 'og:url', content: 'https://lucasbrito.com.br' },
-      { name: 'og:description', content: 'Lucas Brito' },
+      { name: 'og:description', content: 'Lucas Brito - Personal Website. AI, Machine Learning, Data' },
       { name: 'og:site_name', content: 'Lucas Brito' },
       { name: 'og:locale', content: 'pt_BR, en_US' },
       { charset: 'UTF-8' }
