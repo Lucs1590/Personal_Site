24f55cd6b19d5f0d3c38384f6bc2584e165b70d3
Lucas Brito <lucasbsilva29@gmail.com>
2024-02-23 22:49:20 -0300

    fix: aria label

--- src/app/secondary-components/icones/icones.component.ts
+++ src/app/secondary-components/icones/icones.component.ts
@@ -13,61 +13,61 @@ export class IconesComponent {
       icon: ['fas', 'envelope'],
       link: 'mailto:lucasbsilva29@gmail.com?subject=Brito%20Web%20Site&body=Ola%20Lucas%20Brito',
       active: true,
-      descriptiion: 'Send me an e-mail'
+      description: 'Send me an e-mail'
     },
     {
       icon: ['fab', 'github'],
       link: 'https://github.com/Lucs1590',
       active: true,
-      descriptiion: 'Access my GitHub profile'
+      description: 'Access my GitHub profile'
     },
     {
       icon: ['fab', 'stack-overflow'],
       link: 'https://pt.stackoverflow.com/users/122718/lucas-brito',
       active: false,
-      descriptiion: 'Access my Stack Overflow profile'
+      description: 'Access my Stack Overflow profile'
     },
     {
       icon: ['fab', 'medium'],
       link: 'https://medium.com/@lucasbsilva29',
       active: true,
-      descriptiion: 'Access my Medium profile'
+      description: 'Access my Medium profile'
     },
     {
       icon: ['fab', 'dev'],
       link: 'https://dev.to/lucs1590',
       active: true,
-      descriptiion: 'Access my Dev.to profile'
+      description: 'Access my Dev.to profile'
     },
     {
       icon: ['fab', 'twitter'],
       link: 'https://twitter.com/Lucs1590',
       active: true,
-      descriptiion: 'Access my Twitter profile'
+      description: 'Access my Twitter profile'
     },
     {
       icon: ['fab', 'linkedin'],
       link: 'https://www.linkedin.com/in/lucas-brito100/',
       active: true,
-      descriptiion: 'Access my LinkedIn profile'
+      description: 'Access my LinkedIn profile'
     },
     {
       icon: ['fab', 'instagram'],
       link: 'https://www.instagram.com/_lucasbritoo',
       active: true,
-      descriptiion: 'Access my Instagram profile'
+      description: 'Access my Instagram profile'
     },
     {
       icon: ['fab', 'facebook-f'],
       link: 'https://www.facebook.com/lucas.brito.100',
       active: true,
-      descriptiion: 'Access my Facebook profile'
+      description: 'Access my Facebook profile'
     },
     {
       icon: ['fab', 'whatsapp'],
       link: 'https://api.whatsapp.com/send?phone=5514997270606&text=Oi%20Lucas',
       active: false,
-      descriptiion: 'Send me a message on WhatsApp'
+      description: 'Send me a message on WhatsApp'
     },
   ].map(icon => new Icon().deserialize(icon));
 
