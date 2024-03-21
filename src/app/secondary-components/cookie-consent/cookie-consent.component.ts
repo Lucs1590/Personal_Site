2bceb585c1b2519eaf926a489abf345b325e05c8
Lucas Brito <lucasbsilva29@gmail.com>
2024-03-21 20:54:47 -0300

    feat: remove wrong code

--- src/app/secondary-components/cookie-consent/cookie-consent.component.ts
+++ src/app/secondary-components/cookie-consent/cookie-consent.component.ts
@@ -9,7 +9,7 @@ import { CookieService } from 'ngx-cookie-service';
 export class CookieConsentComponent implements OnInit {
   showConsentMessage: boolean = false;
 
-  constructor(private cookieService: CookieService) {}
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit(): void {
     this.checkCookieConsent();
@@ -29,35 +29,4 @@ export class CookieConsentComponent implements OnInit {
   declineConsent(): void {
     this.showConsentMessage = false;
   }
-}
-```
-```html
-<div *ngIf="showConsentMessage" class="cookie-consent-container">
-  <p>We use cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies.</p>
-  <button (click)="acceptConsent()">Accept</button>
-</div>
-```
-```css
-.cookie-consent-container {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
-  color: white;
-  text-align: center;
-  padding: 20px;
-  z-index: 1000;
-}
-
-.cookie-consent-container p {
-  margin: 0 0 20px 0;
-}
-
-.cookie-consent-container button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
 }
