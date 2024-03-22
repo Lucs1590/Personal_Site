3140548bdbb26a0b9d2559557fb9c3c3357c5dfd
Lucas Brito <lucasbsilva29@gmail.com>
2024-03-21 21:53:24 -0300

    refactor: send setLanguage to utils

--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { CookieService } from 'ngx-cookie-service';
+import { firstValueFrom } from 'rxjs';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilsService {
-
-  currentLang = 'pt';
+  currentLang = 'en';
 
   constructor(
     private translate: TranslateService,
     private router: Router,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private apiService: ApiService
   ) { }
 
   useLanguage(): void {
@@ -26,6 +28,29 @@ export class UtilsService {
     }
     if (this.cookieService.check('cookieConsent') && this.cookieService.get('cookieConsent') === 'true') {
       this.cookieService.set('langPref', this.currentLang);
+    }
+  }
+
+
+  async setLanguage(): Promise<void> {
+    this.translate.setDefaultLang(this.currentLang);
+
+    if (this.cookieService.check('cookieConsent') && this.cookieService.get('cookieConsent') === 'true') {
+      const langPref = this.cookieService.get('langPref');
+
+      if (langPref) {
+        this.translate.setDefaultLang(langPref);
+      } else {
+        try {
+          const ipInfo = await firstValueFrom(this.apiService.getIPInfo());
+          const userCountry = ipInfo?.country?.toUpperCase();
+          this.translate.setDefaultLang(userCountry === 'BR' ? 'pt' : 'en');
+        } catch (error) {
+          console.error('Error retrieving IP info:', error);
+          this.translate.setDefaultLang('en');
+        }
+      }
+
     }
   }
 
