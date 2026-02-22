977a6586aff875de4c48321fd7d5c7e88d164a16
Lucas Brito <lucasbsilva29@gmail.com>
2026-02-21 22:21:31 -0300

    feat: update LinkedIn recommendations with new entries and details

--- src/assets/static_data/recommendations.ts
+++ src/assets/static_data/recommendations.ts
@@ -5,52 +5,112 @@ export { LinkedInRecommendation };
 export const linkedInRecommendations: LinkedInRecommendation[] = [
     {
         id: 1,
-        name: 'Samuel Licorio Leiva',
-        role: 'Software Engineer',
-        company: 'TOTVS',
-        relationship: 'Samuel worked with Lucas on different teams',
-        recommendationText: 'Profissional com uma índole admirável, além de um conhecimento técnico apurado e sua empatia impecável, sempre se colocando no lugar dos outros, o que o leva a refletir muito sobre suas atitudes. Isso o torna uma pessoa incrível e extremamente agradável para se trabalhar em equipe. Sou muito grato a Deus por todas as oportunidades profissionais e pessoais em que já convivemos. Grande e fraternal abraço, amado.',
-        date: '2020-04-07',
-        profileImage: 'assets/img/samu.jpeg'
+        name: 'Guilherme Valandro Vivian',
+        role: 'Data Manager | Partner',
+        company: 'Agibank',
+        relationship: 'Guilherme directly supervised Lucas',
+        recommendationText: 'Possui grandes conhecimentos nas seguintes áreas/tecnologias: Machine Learning, SageMaker, SQL, Python, Spark e R Language.',
+        date: '2023-09-12',
+        profileImage: 'assets/img/recommendations/guilherme.jpeg'
     },
     {
         id: 2,
-        name: 'Álvaro Leandro Cavalcante',
-        role: 'PhD Researcher',
-        company: 'UNESP',
-        relationship: 'Lucas and Álvaro worked together in the same team',
-        recommendationText: 'Ótimo profissional, muito dedicado em suas tarefas, visa executar tudo com excelência, se mostrando um profissional muito comprometido com o trabalho.',
-        date: '2019-09-17',
-        profileImage: 'assets/img/alvaro.jpeg'
+        name: 'Allan Siriani',
+        role: 'Advisor | Head of ICT | AI & Deep Learning Leader',
+        company: '—',
+        relationship: 'Allan directly supervised Lucas',
+        recommendationText: 'Rapaz muito dedicado e que realmente busca conhecimento e aplica de forma exemplar, comunicativo e que trabalha de forma focada em atingir as metas. Como aluno não deixou pendências e sempre entrega mais do que o combinado.',
+        date: '2023-02-02',
+        profileImage: 'assets/img/recommendations/allan.jpeg'
     },
     {
         id: 3,
-        name: 'Fernando Maransatto',
-        role: 'Software Engineer',
-        company: 'CI&T',
-        relationship: 'Fernando directly managed Lucas',
-        recommendationText: 'Excelente profissional. Responsável, não abre mão de desafio, encara bem as mudanças de escopo. Tem uma ótima curva de aprendizado, e trabalha muito bem em equipe.',
-        date: '2019-09-18',
-        profileImage: 'assets/img/maransatto.jpeg'
+        name: 'Dennis Nakamura',
+        role: 'Digital Economy Specialist | Co-founder | Mentor',
+        company: '—',
+        relationship: 'Dennis worked with Lucas in different companies',
+        recommendationText: 'Lucas é um excelente profissional. Organizado e extremamente humano, pudemos desenvolver alguns projetos bem inovadores nesses últimos anos da pandemia. Sem dúvidas é uma pessoa para se manter sempre por perto.',
+        date: '2022-08-29',
+        profileImage: 'assets/img/recommendations/dennis.jpeg'
     },
     {
         id: 4,
-        name: 'Renan Zulian',
-        role: 'Software Engineer',
-        company: 'CI&T',
-        relationship: 'Lucas and Renan worked together in the same team',
-        recommendationText: 'Foi e sempre será um prazer dividir uma mesa de trabalho com ele. Uma pessoa dedicada, que tem um ótimo espírito competitivo e que ama desafios. Esse seu estilo sempre motiva os colegas de trabalho a fazer o seu melhor.',
-        date: '2020-02-12',
-        profileImage: 'assets/img/renan.jpeg'
+        name: 'Leonardo Nardi Hüffner',
+        role: 'Experienced Data Professional',
+        company: '—',
+        relationship: 'Leonardo and Lucas worked together in the same team',
+        recommendationText: 'Desde o primeiro dia de trabalho o Lucas trouxe uma enxurrada de ideias e ferramentas novas para melhorar o dia-a-dia do time. Entregas consistentes e com qualidade, além de ser uma ótima companhia!',
+        date: '2022-08-24',
+        profileImage: 'assets/img/recommendations/leonardo.jpeg'
     },
     {
         id: 5,
+        name: 'Jonathan Tsen',
+        role: 'Senior AI Engineer',
+        company: 'Labrynth.ai',
+        relationship: 'Jonathan and Lucas worked together in the same team',
+        recommendationText: 'Lucas is an excellent professional. He thinks of things other team members do not usually think about. As a result, he helps uncover insights and ideas for new projects. He has programming skills, business skills, a creative view of the world, and an incredible storytelling ability.',
+        date: '2021-08-12',
+        profileImage: 'assets/img/recommendations/jonathan.jpeg'
+    },
+    {
+        id: 6,
+        name: 'Stephen Kunihiro',
+        role: 'CEO',
+        company: '—',
+        relationship: 'Stephen directly supervised Lucas',
+        recommendationText: 'Valores morais e éticos consistentes, esforçado, dedicado, centrado, amigável e humilde.',
+        date: '2021-02-15',
+        profileImage: 'assets/img/recommendations/stephen.jpeg'
+    },
+    {
+        id: 7,
         name: 'Rafael Petraca',
-        role: 'Software Engineer',
-        company: 'CI&T',
-        relationship: 'Lucas and Rafael worked together in the same team',
+        role: 'Data Analyst | Power BI | SQL Server',
+        company: '—',
+        relationship: 'Rafael and Lucas worked together in the same team',
         recommendationText: 'Ótimo profissional, com valores e ética. Sempre compartilhando conhecimento com os demais! E acima de tudo um grande amigo.',
         date: '2021-02-02',
-        profileImage: 'assets/img/rafa.jpeg'
+        profileImage: 'assets/img/recommendations/rafael.jpeg'
+    },
+    {
+        id: 8,
+        name: 'Samuel Licorio Leiva',
+        role: 'UX Analyst',
+        company: 'Grupo Jacto',
+        relationship: 'Samuel and Lucas worked in different teams',
+        recommendationText: 'Profissional com uma índole admirável, além de um conhecimento técnico apurado e sua empatia impecável, sempre se colocando no lugar dos outros, o que o leva a refletir muito sobre suas atitudes. Isso o torna uma pessoa incrível e extremamente agradável para se trabalhar em equipe. Sou muito grato a Deus por todas as oportunidades profissionais e pessoais em que já convivemos. Grande e fraternal abraço, amado.',
+        date: '2020-04-07',
+        profileImage: 'assets/img/recommendations/samuel.jpeg'
+    },
+    {
+        id: 9,
+        name: 'Renan Zulian',
+        role: 'FullStack Developer | Backend Developer | AWS',
+        company: '—',
+        relationship: 'Renan and Lucas worked together in the same team',
+        recommendationText: 'Foi e sempre será um prazer dividir uma mesa de trabalho com ele. Uma pessoa dedicada, que tem um ótimo espírito competitivo e que ama desafios. Esse seu estilo sempre motiva os colegas de trabalho a fazer o seu melhor.',
+        date: '2020-02-11',
+        profileImage: 'assets/img/recommendations/renan.jpeg'
+    },
+    {
+        id: 10,
+        name: 'Fernando Silva Maransatto',
+        role: 'Senior Full-Stack Developer',
+        company: 'X-Team',
+        relationship: 'Fernando reported directly to Lucas',
+        recommendationText: 'Excelente profissional. Responsável, não abre mão de desafio, encara bem as mudanças de escopo. Tem uma ótima curva de aprendizado e trabalha muito bem em equipe.',
+        date: '2019-09-18',
+        profileImage: 'assets/img/recommendations/fernando.jpeg'
+    },
+    {
+        id: 11,
+        name: 'Álvaro Leandro Cavalcante Carneiro',
+        role: 'Data & AI Specialist | MSc. Computer Science',
+        company: '—',
+        relationship: 'Álvaro and Lucas worked together in the same team',
+        recommendationText: 'Ótimo profissional, muito dedicado em suas tarefas, visa executar tudo com excelência, se mostrando um profissional muito comprometido com o trabalho.',
+        date: '2019-09-16',
+        profileImage: 'assets/img/recommendations/alvaro.jpeg'
     }
 ];
